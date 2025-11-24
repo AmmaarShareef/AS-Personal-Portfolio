d9732210603cef6e3e1b2415485f13eb0a8fcfc0
Ammaar Shareef <ammhood1602@gmail.com>
2025-11-24 16:36:40 -0500

    Fix GitHub Pages by using relative base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AS-Personal-Portfolio/'  // <-- exact repo name
+  base: '/AS-Personal-Portfolio/'
 })
